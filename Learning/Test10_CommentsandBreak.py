984ddc07c0ab5e6c5f77e47405fc0936b3bc503e
Administrator <chinalzge@gmail.com>
2017-03-08 00:33:34 +0800

    add break+continue+functions

---
+++ Learning/Test10_CommentsandBreak.py
@@ -0,0 +1,12 @@
+'''
+Python的注释
+'''
+
+magicNumber=25
+
+for i in range(101):
+    if i is magicNumber:
+        print(i, "is the magic Number!")
+        break
+    else:
+        print(i)
---
+++ Learning/Test12_Functions.py
@@ -0,0 +1,21 @@
+'''  Functions:Python中的方法'''
+
+
+def beef():
+    print("Dayum,Functions are cool!")
+
+
+#     调用方法
+beef()
+
+print("----------------------------")
+
+
+# 比特币转换方法
+def bitcoin_to_usd(btc):
+    amount=btc*527
+    print(amount)
+
+bitcoin_to_usd(1)
+bitcoin_to_usd(2)
+bitcoin_to_usd(3)
